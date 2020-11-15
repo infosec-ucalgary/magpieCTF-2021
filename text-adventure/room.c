b2ad172acbeed3aa8ff725c3b119cca39200fb69
E-Baird <emilyiris.baird@gmail.com>
2020-11-15 15:56:55 -0700

    Added 'clear' functionality and cleaned up output

--- text-adventure/room.c
+++ text-adventure/room.c
@@ -27,11 +27,11 @@ struct room ** initializeMap() {
     // create all game rooms 
     struct room ** map = (struct room **) malloc(5 * sizeof(struct room *));
 
-    map[0] = initializeRoom("entrance", "You're in a room, but you don't know how you got here\n");
-    map[1] = initializeRoom("main", "You are in a large space with doors to the N, S, E, and W\n");
-    map[2] = initializeRoom("lab", "Broken computer equipment is everywhere, but one terminal continues to blink...\n");
-    map[3] = initializeRoom("locked", "A locked safe sits on the north side of the room, protected by a complex-looking pin pad.\n");
-    map[4] = initializeRoom("secret", "You're not sure you're supposed to be here...\n");
+    map[0] = initializeRoom("entrance", "You're in a room, but you don't know how you got here");
+    map[1] = initializeRoom("main", "You are in a large space with doors to the N, S, E, and W");
+    map[2] = initializeRoom("lab", "Broken computer equipment is everywhere, but one terminal continues to blink...");
+    map[3] = initializeRoom("locked", "A locked safe sits on the north side of the room, protected by a complex-looking pin pad.");
+    map[4] = initializeRoom("secret", "You're not sure you're supposed to be here...");
 
     // create links between rooms
     map[0]->northLink = map[1];
@@ -54,50 +54,50 @@ struct room ** initializeMap() {
 void executeGo(const char *noun, struct room *currRoom) {
     if (noun != NULL && strcmp(noun, "north") == 0) {
         if (currRoom->northLink == NULL){
-            printf("You can't go that way in here.\n");
+            printf("You can't go that way in here.");
         } 
         else {
             *currRoom = *currRoom->northLink;
-            printf("%s\n", currRoom->description);
+            printf("%s", currRoom->description);
         }
     }
     else if (noun != NULL && strcmp(noun, "south") == 0) {
         if (currRoom->southLink == NULL){
-            printf("You can't go that way in here.\n");
+            printf("You can't go that way in here.");
         }
         else {
             *currRoom = *currRoom->southLink;
-            printf("%s\n", currRoom->description);
+            printf("%s", currRoom->description);
         }
     }
     else if (noun != NULL && strcmp(noun, "east") == 0) {
         if (currRoom->eastLink == NULL){
-            printf("You can't go that way in here.\n");
+            printf("You can't go that way in here.");
         }
         else {
             *currRoom = *currRoom->eastLink;
-            printf("%s\n", currRoom->description);
+            printf("%s", currRoom->description);
         }
     }
     else if (noun != NULL && strcmp(noun, "west") == 0) {
         if (currRoom->westLink == NULL){
-            printf("You can't go that way in here.\n");
+            printf("You can't go that way in here.");
         }
         else {
             *currRoom = *currRoom->westLink;
-            printf("%s\n", currRoom->description);
+            printf("%s", currRoom->description);
         }
     }
     else {
-      printf("I don't understand where you want to go.\n");
+      printf("I don't understand where you want to go.");
    }
 }
 
 void executeLook(const char *noun, struct room *currRoom) {
     if (noun != NULL && strcmp(noun, "around") == 0) {
-      printf("%s\n", currRoom->description);
+      printf("%s", currRoom->description);
    }
    else {
-      printf("I don't understand what you want to see.\n");
+      printf("I don't understand what you want to see.");
    }
 }
