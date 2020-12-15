08ca767c5da490405319e9465ae0d053c047ec07
James Lowther <jamesevlowther@gmail.com>
2020-12-15 13:09:42 -0700

    Added challenges and writeups

---
+++ challenges/web-exploitation/sweatin-in-latex/source/public_html/config.php
@@ -0,0 +1,6 @@
+<?php
+$PDFLATEX = "/usr/bin/pdflatex";
+$COMPILEDIR = __DIR__ . "/compile/";
+$OUTPUTDIR = __DIR__ . "/pdf/";
+$DLURL = "http://" . $_SERVER['SERVER_NAME'] . ":" . $_SERVER['SERVER_PORT'] . "/pdf/";
+?>
