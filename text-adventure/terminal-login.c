3ca5b3b064b5c494b1c0b3a30efbbd7c0f7c7399
infosecucalgary <60866282+infosecucalgary@users.noreply.github.com>
2020-12-29 22:21:52 -0700

    Update terminal-login.c
    The bare-bones exploitable. More lore stuff to add tomorrow.

--- text-adventure/terminal-login.c
+++ text-adventure/terminal-login.c
@@ -1,16 +1,31 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<string.h>
 
-void printflag(){
-    printf("flag!\n");
-}
+int main() {
 
 
-int main(){
-    printf("Give me a number\n");
+	char password[] = "something";
 
-    char buf[10];
-    gets(buf, stdin);
+	char buf[256];
 
-    printf("Thanks!\n");
+	gets(buf);
+
+	printf("pas: %s\n", password);
+	printf("buf: %s\n", buf);
+	int res;
+
+	res = strcmp(password, buf);
+
+	printf("%d", res);
+
+	if (res == 0) {
+		printf("welcome\n"); 
+	}
+	else {
+		printf("wrong password\n");
+	}
+
+	return 1;
+
+
 }
