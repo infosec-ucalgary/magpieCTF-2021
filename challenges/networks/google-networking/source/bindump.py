3ea3d74a76e8f544d057fcd3ab91e9f366d075be
Ethan Sengsavang <eth.sengsavang@gmail.com>
2020-12-31 09:57:06 -0700

    Fully automate file creation and add adjustability

--- challenges/networks/google-networking/source/bindump.py
+++ challenges/networks/google-networking/source/bindump.py
@@ -6,8 +6,19 @@ import random
 
 def main():
     dump = "./bin/"
+    file_count = 50 # this will take up (file_count / 10) GB of storage
 
-    for i in range(1,100):
+    try:
+        os.mkdir(dump)
+        print(f"Created {dump}, which will house all {file_count} bin files");
+    except FileExistsError:
+        print(f"Found pre-existing {dump}, clearing...")
+
+        binfiles = [f for f in os.listdir(dump) if f.endswith(".bin")]
+        for f in binfiles:
+            os.remove(f"{dump}{f}")
+
+    for i in range(1, file_count):
         binname = f"{hex(random.randint(i* 10000000000, i * 10000000000))[2::]}.bin"
         rand_dump = b"\x00" + os.urandom(100000000) + b"\x00"
 
