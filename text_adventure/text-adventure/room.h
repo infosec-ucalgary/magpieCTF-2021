db9cca3c67277b04b441cfefad4ef43c8ced1e0f
E-Baird <emilyiris.baird@gmail.com>
2021-01-10 16:45:07 -0700

    Add text_adventure to misc challenges

--- text_adventure/text-adventure/room.h
+++
@@ -1,7 +0,0 @@
-extern struct room * initializeRoom(char *name, char *description);
-extern struct room ** initializeMap();
-
-extern void executeLook(const char *noun, struct room *currRoom);
-extern void executeGo(const char *noun, struct room *currRoom);
-extern void executeUse(const char *noun, struct room *currRoom);
-
