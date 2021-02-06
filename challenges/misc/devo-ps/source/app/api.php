9a3fc535064fc0c3b31b62bd034b5bf94456dad2
E-Baird <emilyiris.baird@gmail.com>
2021-02-06 15:31:12 -0700

    change devo-ps to use cookie instead of auth header

--- challenges/misc/devo-ps/source/app/api.php
+++ challenges/misc/devo-ps/source/app/api.php
@@ -1,21 +1,9 @@
 <?php
 
-$headers =  getallheaders();
-
-foreach($headers as $key=>$val){
-
-    if ($key == "Authorization") {
-        if ($val != "token f4e53e561c6580d6d304f3f31e3102f5") {
-            echo '{"status":400,"data":"Invalid Request"}';
-            exit(0);
-        } 
-        else {
-            echo'magpie{build_automation_genius}';
-            exit(0);
-        }
-    }
+if(isset($_COOKIE['auth']) && $_COOKIE['auth'] == 'f4e53e561c6580d6d304f3f31e3102f5') {
+    echo'magpie{build_automation_genius}';
+} else {
+    echo '{"status":400,"data":"Invalid Request"}';
 }
-
-echo '{"status":400,"data":"Invalid Request"}';
 
 ?> 
--- challenges/misc/devo-ps/source/app/flaghash.sh
+++ challenges/misc/devo-ps/source/app/flaghash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # TODO: This URL should be changed to reflect the correct endpoint URL when challenge is hosted
-FLAG="`curl http://web.magpiectf.ca:4325/ --header 'Authorization: token f4e53e561c6580d6d304f3f31e3102f5'`"
+FLAG="`curl -b 'auth=f4e53e561c6580d6d304f3f31e3102f5' http://srv3.magpiectf.ca:9355/api.php`"
 echo $( md5sum <<< $FLAG | cut -d " " -f1)
 
